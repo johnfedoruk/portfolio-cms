5d67d482b20272a35c9e4541469e28c1dfe3f83c
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-06-30 19:05:54 +0100

    Added jQuery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+	ngOnInit(): void {
+		$("#title").fadeOut(1000);
+		setTimeout(()=>{
+			$("#title").fadeIn(1000);
+		},1000);
+	}
+	title = 'app works!';
 }
