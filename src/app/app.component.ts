3833523496ebbafe471e7d5fc1d73e21965a4798
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-06-30 20:40:58 +0100

    Hardcoded sidenav into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,21 @@ import * as $ from 'jquery';
 declare const jQuery: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 	ngOnInit(): void {
-		// $("#title").fadeOut(1000);
-		// setTimeout(()=>{
-		// 	$("#title").fadeIn(1000);
-		// },1000);
-		jQuery('.carousel').carousel();
+		jQuery('.button-collapse').sideNav({
+			menuWidth: 300, // Default is 300
+			edge: 'left', // Choose the horizontal origin
+			closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+			draggable: true, // Choose whether you can drag to open on touch screens,
+		});
 	}
 	title = 'app works!';
+	public openSidenav(): void {
+
+	}
 }
