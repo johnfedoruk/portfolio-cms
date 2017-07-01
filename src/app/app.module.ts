a3ccd51c0a8efc496ea015c680015afff89815e7
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-07-01 11:58:31 +0100

    Added RouterModule and registered /home with new HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './partials/side-menu/side-menu.component';
 import { TopMenuComponent } from './partials/top-menu/top-menu.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes:Routes = [
+	{
+		path:"",
+		component:HomeComponent
+	}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SideMenuComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
