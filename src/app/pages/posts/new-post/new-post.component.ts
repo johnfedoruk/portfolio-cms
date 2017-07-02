9c589956e4afa658eabb8c52bf760a903230587f
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-07-02 17:36:21 +0100

    Empty modal in NewPostComponent to select or create an album

--- src/app/pages/posts/new-post/new-post.component.ts
+++ src/app/pages/posts/new-post/new-post.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const jQuery: any;
+
+const NEW_ALBUM:string = "NEW_ALBUM";
+const SEL_ALBUM:string = "SEL_ALBUM";
+
 @Component({
-  selector: 'app-new-post',
-  templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+	selector: 'app-new-post',
+	templateUrl: './new-post.component.html',
+	styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+	public album_input_type:string = NEW_ALBUM;
+	public NEW_ALBUM:string = NEW_ALBUM;
+	public SEL_ALBUM:string = SEL_ALBUM;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	public openAlbumModal(): void {
+		jQuery('#albumModal').modal('open');
+	}
+
+	public onOpenNewAlbum():void {
+		this.album_input_type = NEW_ALBUM;
+	}
+
+	public onOpenSelectAlbum():void {
+		this.album_input_type = SEL_ALBUM;
+	}
+
+	public onSaveAlbum():void {
+
+		jQuery('#albumModal').modal('close');
+	}
+
+	public onSelectAlbum():void {
+		
+		jQuery('#albumModal').modal('close');
+	}
 
 }
