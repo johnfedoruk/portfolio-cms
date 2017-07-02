bd897685735a33f36924abbaacf718b577d21309
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-07-02 18:03:10 +0100

    Added some structure to New Album page

--- src/app/pages/albums/new-album/new-album.component.ts
+++ src/app/pages/albums/new-album/new-album.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const jQuery:any;
+
 @Component({
-  selector: 'app-new-album',
-  templateUrl: './new-album.component.html',
-  styleUrls: ['./new-album.component.css']
+	selector: 'app-new-album',
+	templateUrl: './new-album.component.html',
+	styleUrls: ['./new-album.component.css']
 })
 export class NewAlbumComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	public openPostModal():void {
+		jQuery('#postModal').modal('open');
+	}
+
+	public onSelectPost():void {
+		jQuery('#postModal').modal('close');
+	}
 
 }
